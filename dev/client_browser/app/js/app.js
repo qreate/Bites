3d94a98895f5a687f7a9732c17f9967b26dfa4f9
Robert Hjalmers <robert.hjalmers@gmail.com>
2014-08-02 08:49:40 +0200

    working add bite, list bite, show bite, facebook oauth

--- dev/client_browser/app/js/app.js
+++ dev/client_browser/app/js/app.js
@@ -7,11 +7,19 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'facebook'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'authenticationCtrl'});
   $routeProvider.when('/view1', {templateUrl: 'partials/add-bite.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/list-bites.html', controller: 'MyCtrl2'});
   $routeProvider.when('/bite/:biteId', {templateUrl: 'partials/show-bite.html', controller: 'MyController'});
-  //$routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]).
+config(['FacebookProvider', function(FacebookProvider) {
+    // Here you could set your appId through the setAppId method and then initialize
+    // or use the shortcut in the initialize method directly.
+        var myAppId = '916383501710710';
+        FacebookProvider.init(myAppId);
 }]);
--- dev/client_browser/app/js/controllers.js
+++ dev/client_browser/app/js/controllers.js
@@ -53,6 +53,9 @@ angular.module('myApp.controllers', ['myApp.services'])
   }])
   .controller('NavCtrl1', ['$scope', function($scope, navigation) {
         $scope.navigation = [{
+            name:"Login",
+            hash:"login"
+        },{
             name:"Add Bite",
             hash:"view1"
         },{
@@ -117,5 +120,134 @@ angular.module('myApp.controllers', ['myApp.services'])
         var bite = bites.showBite();
         //alert(JSON.stringify(bites.showBite()))
         $scope.bites = bite[0];
-}]);
+  }])
+  .controller('authenticationCtrl', ['$scope','$timeout','Facebook', function($scope, $timeout, Facebook) {
+
+            // Define user empty data :/
+            $scope.user = {};
+
+            // Defining user logged status
+            $scope.logged = false;
+
+            // And some fancy flags to display messages upon user status change
+            $scope.byebye = false;
+            $scope.salutation = false;
+
+            /**
+             * Watch for Facebook to be ready.
+             * There's also the event that could be used
+             */
+            $scope.$watch(
+                function() {
+                    return Facebook.isReady();
+                },
+                function(newVal) {
+                    if (newVal)
+                        $scope.facebookReady = true;
+                }
+            );
+
+            /**
+             * IntentLogin
+             */
+            $scope.IntentLogin = function() {
+                Facebook.getLoginStatus(function(response) {
+                    if (response.status == 'connected') {
+                        $scope.logged = true;
+                        $scope.me();
+                    }
+                    else
+                        alert("ok3");
+                        $scope.login();
+                });
+            };
+
+            /**
+             * Login
+             */
+            $scope.login = function() {
+                Facebook.login(function(response) {
+                    if (response.status == 'connected') {
+                        $scope.logged = true;
+                        $scope.me();
+                    }
+
+                });
+            };
+
+            /**
+             * me
+             */
+            $scope.me = function() {
+                Facebook.api('/me', function(response) {
+                    /**
+                     * Using $scope.$apply since this happens outside angular framework.
+                     */
+                    $scope.$apply(function() {
+                        $scope.user = response;
+                    });
+
+                });
+            };
+
+            /**
+             * Logout
+             */
+            $scope.logout = function() {
+                Facebook.logout(function() {
+                    $scope.$apply(function() {
+                        $scope.user   = {};
+                        $scope.logged = false;
+                    });
+                });
+            }
+
+            /**
+             * Taking approach of Events :D
+             */
+            $scope.$on('Facebook:statusChange', function(ev, data) {
+                console.log('Status: ', data);
+                if (data.status == 'connected') {
+                    $scope.$apply(function() {
+                        $scope.salutation = true;
+                        $scope.byebye     = false;
+                    });
+                } else {
+                    $scope.$apply(function() {
+                        $scope.salutation = false;
+                        $scope.byebye     = true;
+
+                        // Dismiss byebye message after two seconds
+                        $timeout(function() {
+                            $scope.byebye = false;
+                        }, 2000)
+                    });
+                }
+
+
+            });
+
+
+        }
+    ])
+
+/**
+ * Just for debugging purposes.
+ * Shows objects in a pretty way
+ */
+    .directive('debug', function() {
+        return {
+            restrict:	'E',
+            scope: {
+                expression: '=val'
+            },
+            template:	'<pre>{{debug(expression)}}</pre>',
+            link:	function(scope) {
+                // pretty-prints
+                scope.debug = function(exp) {
+                    return angular.toJson(exp, true);
+                };
+            }
+        }
+    });
 
